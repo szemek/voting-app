279a4e9d5da234fa394820c283885fdd7429f088
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2015-12-26 12:39:42 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+question1 = Question.find_or_initialize_by(title: 'SQL vs. no-SQL')
+question1.options = [
+  Option.find_or_initialize_by(question_id: question1.id, title: 'SQL'),
+  Option.find_or_initialize_by(question_id: question1.id, title: 'no-SQL')
+]
+question1.save
+
+question2 = Question.find_or_initialize_by(title: 'iOS vs. Android vs. Windows Phone')
+question2.options = [
+  Option.find_or_initialize_by(question_id: question2.id, title: 'iOS'),
+  Option.find_or_initialize_by(question_id: question2.id, title: 'Android'),
+  Option.find_or_initialize_by(question_id: question2.id, title: 'Windows Phone')
+]
+question2.save
