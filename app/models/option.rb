7f8d300f1a615ba3869ae15fe43992b727f4476d
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2015-12-26 12:24:28 +0100

    validations, custom error messages

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,3 +1,4 @@
 class Option < ActiveRecord::Base
   belongs_to :question
+  validates_presence_of :title
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ActiveRecord::Base
   has_many :options
   accepts_nested_attributes_for :options, reject_if: :all_blank, allow_destroy: true
+  validates_presence_of :title
   validates_presence_of :options
 end
