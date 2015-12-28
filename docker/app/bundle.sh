6dfb06747d5da64d973ec613832ab08f8062646d
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2015-12-28 22:34:19 +0100

    install only gems for running application in production environment

--- docker/app/bundle.sh
+++ docker/app/bundle.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 cd /home/app/voting-app
-bundle check || bundle install
+bundle check || bundle install --without development test --deployment
 bundle exec rake db:migrate
 bundle exec rake db:seed
 bundle exec rake assets:precompile
